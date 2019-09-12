7a13e9c3b203bd70e8a0a27bf0b76b8db0e32ba2
Yi Dang <Yi@Yis-MacBook-Pro.local>
2019-09-12 18:31:29 -0400

    complete half of question 6

--- A1_T4.py
+++ A1_T4.py
@@ -1,5 +1,7 @@
 pressure_arr = [80, 90, 100, 150, 120, 110, 160, 110, 100]
 
 for x in pressure_arr:
-    print (x/9)
-    
+    pressure = pressure_arr[x]
+    mean_pressure = pressure/9
+
+print (mean_pressure)
---
+++ A1_T6.py
@@ -0,0 +1,17 @@
+#1 Given array data, find single mode value
+
+arr_1 = [80, 90, 100, 150, 120, 110, 160, 110, 100]
+
+print(arr_1[0])
+
+#2 Given array data, find multiple mode values
+
+arr_2 = [4, 5, 2, 6, 3, 7, 8, 9, 6, 5, 2]
+
+print(arr_2[1], arr_2[4])
+
+#3 Given array data, find range
+
+arr_3 = [21, 23, 26, 22, 25, 20, 19, 23]
+
+print(arr_3[2:7])
